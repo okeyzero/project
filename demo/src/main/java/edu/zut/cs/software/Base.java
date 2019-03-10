12b88b0bd6227fd3a2cb097d653d7745d9af7e11
ys <576925583@qq.com>
2019-03-10 22:12:10 +0800

    test

---
+++ demo/src/main/java/edu/zut/cs/software/Base.java
@@ -0,0 +1,10 @@
+package edu.zut.cs.software;
+
+public class Base implements BaseService {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ demo/src/test/java/edu/zut/cs/software/BaseTest.java
@@ -0,0 +1,14 @@
+package edu.zut.cs.software;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BaseTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
