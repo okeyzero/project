02289fcdee842a91e1114b11ea5364a777aa3cfa
ys <576925583@qq.com>
2019-03-11 22:33:37 +0800

    commit sun

---
+++ sun/sun-service/src/main/java/edu/zut/cs/software/sun/admin/service/Calculator.java
@@ -0,0 +1,20 @@
+package edu.zut.cs.software.sun.admin.service;
+
+public class Calculator {
+public int add(int a,int b)
+{
+	return a+b;
+}
+public int substract(int a,int b)
+{
+	return a-b;
+}
+public int multiply(int a,int b)
+{
+	return a*b;
+}
+public int divide(int a,int b)
+{
+	return a/b;
+}
+}
---
+++ sun/sun-service/src/test/java/edu/zut/cs/software/sun/admin/service/CalculatorTest.java
@@ -0,0 +1,14 @@
+package edu.zut.cs.software.sun.admin.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
